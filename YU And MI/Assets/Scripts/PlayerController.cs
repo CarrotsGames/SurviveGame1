e2c2e4ec1095bb96d192dc3c5a5fae43bd394a9c
Gabriel P <gaberofl42@gmail.com>
2019-02-14 13:52:31 +1100

    Glide
    Added Glideing to player2(Yu)

--- YU And MI/Assets/Scripts/PlayerController.cs
+++ YU And MI/Assets/Scripts/PlayerController.cs
@@ -11,7 +11,6 @@ public class PlayerController : MonoBehaviour {
     public float AccelerationTimeAirBourne = .2f;
     public float AccelerationTimeGrounded = .1f;
     public float MoveSpeed = 6;
-    public bool GLIDING = false;
 
     // public Vector2 WallJumpClimb;
     // public Vector2 WallJumpOff;
@@ -20,19 +19,28 @@ public class PlayerController : MonoBehaviour {
 
     public float WallSlideSpeedMax;
 
-
+    public float TimeTillGlide;
+    float GlideTimer;
     public float Gravity;
     public float JumpVelocity;
     public bool CanJump;
     bool CanGlide;
-    Vector3 Velocity;
+    float Apex;
+    float grav;
+    int Gliding = 0;
+
+     Vector3 Velocity;
     PlayerPhysics playerphys;
     private void Start()
     {
         playerphys = GetComponent<PlayerPhysics>();
+        Apex = TimeToApex;
         Gravity = -(2 * JumpHeight) / Mathf.Pow(TimeToApex, 2);
+        grav = Gravity;
+        GlideTimer = TimeTillGlide;
         JumpVelocity = Mathf.Abs(Gravity) * TimeToApex;
         CanJump = true;
+        CanGlide = false;
     }
 
     void Update()
@@ -53,7 +61,9 @@ public class PlayerController : MonoBehaviour {
 
         if (playerphys.Collisions.Above || playerphys.Collisions.Below)
         {
-
+           // Gliding = 0;
+           // Gravity = grav;
+           // TimeToApex = Apex;
             Velocity.y = 0;
         }
         if ((playerphys.Collisions.Left) && !playerphys.Collisions.Below && Velocity.y < 0)
@@ -79,7 +89,7 @@ public class PlayerController : MonoBehaviour {
                 Velocity.y = WallJump.y;
             }
         }
-        if (XCI.GetButton(XboxButton.A))
+        if (XCI.GetButtonDown(XboxButton.A))
         {
       
             if (playerphys.Collisions.Above)
@@ -87,28 +97,65 @@ public class PlayerController : MonoBehaviour {
                 Velocity.y = JumpVelocity;
             }
         }
-        if (tag == "Player2")
+         if (tag == "Player2")
         {
-            if (XCI.GetButtonDown(XboxButton.A) && !CanGlide)
+            if (CanGlide)
+            {
+                TimeTillGlide -= Time.deltaTime;
+ 
+                if (TimeTillGlide <= 0)
+                {
+                    TimeToApex = 1;
+                    Gravity = -(2 * JumpHeight) / Mathf.Pow(TimeToApex, 2);
+                }
+            }
+             if (XCI.GetButtonDown(XboxButton.A))
             {
                 CanGlide = true;
+              
             }
-            if(CanGlide && XCI.GetButtonDown(XboxButton.A))
+            if (XCI.GetButtonUp(XboxButton.A))
             {
+                TimeTillGlide = GlideTimer;
+                TimeToApex = Apex;
+                 Gravity = -(2 * JumpHeight) / Mathf.Pow(TimeToApex, 2);
+                CanGlide = false;
 
-                if(XCI.GetButtonUp(XboxButton.A))
-                GLIDING = true;
+
             }
-            if (XCI.GetButtonDown(XboxButton.A) && GLIDING)
-            {
-                Debug.Log("Glide");
-            }
+
+            //  if(XCI.GetButtonDown(XboxButton.A) && Gliding <= 1)
+            //  {
+            //      Gliding++;
+            //  }
+            //  switch (Gliding)
+            //  {
+            //
+            //      case 2:
+            //          if (XCI.GetButtonDown(XboxButton.A))
+            //          {
+            //              TimeToApex = 2;
+            //              Gravity = -(2 * JumpHeight) / Mathf.Pow(TimeToApex, 2);
+            //              Debug.Log("Gliding");
+            //          }
+            //          if (XCI.GetButtonUp(XboxButton.A))
+            //          {
+            //              TimeToApex = Apex;
+            //              Gravity = -(2 * JumpHeight) / Mathf.Pow(TimeToApex, 2);
+            //          }
+            //          else  if (playerphys.Collisions.Above || playerphys.Collisions.Below)
+            //          {
+            //              TimeToApex = Apex;
+            //              Debug.Log("Grounded");
+            //              Velocity.y = 0;
+            //          }
+            //          break;
+            //
+            //  }
+
+
         }
-         
-        if (tag == "Player")
-        {
-            CanGlide = false;
-        }
+
 
         Velocity.y += Gravity * Time.deltaTime;
         playerphys.Move(Velocity * Time.deltaTime);
