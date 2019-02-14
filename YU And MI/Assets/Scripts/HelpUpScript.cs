c5f80e77ef67cbb768fa35ff218f9191feb2adbe
Gabriel P <gaberofl42@gmail.com>
2019-02-14 16:39:19 +1100

    Added Scripts and Changed values
    Added the HelpUpScript to help the other player onto a ledge(not complete yet)
    Changed Values with Yu's glide

---
+++ YU And MI/Assets/Scripts/HelpUpScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpUpScript : MonoBehaviour
+{
+    float a;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+         
+           if (collision.gameObject.tag == "Player")
+            {
+                Debug.Log("SupFren2");
+            }
+            if (collision.gameObject.tag == "Player2")
+            {
+                Debug.Log("SupFren1");
+            }
+         
+    }
+}
